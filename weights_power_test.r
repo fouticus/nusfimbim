214b28b3fa4a417f5cbb90412ae45eb43de8545c
Alex Fout <fout.alex@gmail.com>
2020-02-11 17:26:03 -0700

    changed so can run in parallel, also added more powers

--- weights_power_test.r
+++ weights_power_test.r
@@ -11,6 +11,7 @@
 rm(list=ls())
 
 source("functions.r")
+library(parallel)
 library(reshape2)
 library(ggplot2)
 
@@ -31,6 +32,12 @@ diag_stat <- function(X){
   return(sum(abs(ones[,1] - ones[,2])))
 }
 
+# sampling parameters
+N <- 1000   # number of samples per weight
+burnin <- 1000  # burn in before sampling
+thin <- 1000   # thinning
+ps <- c(-10, -8, -4, -2, -1, -0.5, 0, 0.5, 1, 2, 4, 6, 8, 10)  # which powers to evaluate
+
 ########################
 ####### Sampling #######
 ########################
@@ -45,20 +52,22 @@ A <- rbind(cbind(zeros, ones), cbind(ones, zeros)); heat(A)
 ggsave(pu("A.png"), path=output_dir, height=4, width=5)
 W <- outer(1:n, 1:m, FUN=function(x, y){n+m-abs(x-y)})/(n+m); heat(W)
 ggsave(pu("W.png"), path=output_dir, height=4, width=5)
+#W1 <- outer(1:n, 1:m, FUN=function(x, y){n+m-abs(x-y)})/(n+m); heat(W1)
+#ggsave(pu("W1.png"), path=output_dir, height=4, width=5)
+#W2 <- outer(1:n, 1:m, FUN=function(x, y){abs(x-y)})/(n+m)+0.5; heat(W2)
+#ggsave(pu("W2.png"), path=output_dir, height=4, width=5)
 
 
 # sample:
-N <- 1000
-thin <- 1000
 
-ps <- c(-10, -8, -4, -2, 0, 2, 4, 6, 8, 10)
-samples <- list()
-for(i in 1:length(ps)){
-  p <- ps[i]
+sim <- function(p){
   cat(paste("p =", p, "\t"))
   A2 <- A
   W2 <- W^p/max(W^p)
   As <- array(0, dim=c(n, m, N))
+  for(k in 1:burnin){
+    A2 <- swap(A2, W2)
+  }
   for(j in 1:N){
     if(j %% round(N/50) == 0){cat(".")}
     for(k in 1:thin){
@@ -67,26 +76,21 @@ for(i in 1:length(ps)){
     As[,,j] <- A2
   }
   cat("\n")
-  samples[[i]] <- As
+  
+  # compute summary stats
+  stats <- apply(As, 3, diag_stat)
+  df <- data.frame(p=as.factor(rep(p,N)), iter=1:N, stat=stats)
+  write.csv(df, file.path(output_dir, pu(p, "df.csv")))
+  return(df)
 }
 
+dfs <- mclapply(ps, sim, mc.cores=1)
+df <- do.call(rbind, dfs)
 
-# compute summary stats for each state
-stats <- list()
-Np <- N*length(ps)
-df <- data.frame(p=numeric(Np), iter=integer(Np), stat=numeric(Np))
-for(i in 1:length(ps)){
-  p <- ps[i]
-  stats[[i]] <- apply(samples[[i]], 3, diag_stat)
-  df[((i-1)*N+1):(i*N),] <- cbind(rep(p,N), 1:N, stats[[i]])
-  cbind(rep(p,N), 1:N, stats[[i]])
-}
-df$p <- as.factor(df$p)
-write.csv(df, file.path(output_dir, pu("df.csv")))
 
 # Visualize with histogram:
 ggplot(df, aes(x=stat, y=..count.., fill=p)) + 
-  geom_histogram(alpha=0.5, color="black", bins=100, position="identity") +
+  geom_histogram(alpha=0.4, color="black", bins=100, position="identity") +
   labs(title=bquote('Sampling under weights'~W^p), x="Statistic", y="Frequency")
-ggsave(pu("sample.png"), path="output", width=8, height=5)
+ggsave(pu("sample.png"), path="output", width=12, height=5)
 
