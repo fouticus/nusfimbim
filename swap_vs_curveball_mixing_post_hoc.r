d8e4381d1aa029aee1f18006275cad9c983da78a
Alex Fout <fout.alex@gmail.com>
2020-02-21 12:27:46 -0700

    added effective sample size calculation

--- swap_vs_curveball_mixing_post_hoc.r
+++ swap_vs_curveball_mixing_post_hoc.r
@@ -1,6 +1,6 @@
-# File: swap_vs_curveball_mixing.r
+# File: swap_vs_curveball_mixing_post_hoc.r
 # Author: Alex Fout (fout@colostate.edu)
-# Purpose: Compare weighted swapping to curveball in mixing
+# Purpose: Compare weighted swapping to curveball in mixing (post hoc analysis)
 
 
 #####################
@@ -34,9 +34,11 @@ df_files <- lf[grep("df.csv", lf)]
 dfs <- list()
 for(i in 1:length(df_files)){
   df_file <- df_files[[i]]
+  print(df_file)
   params <- strsplit(df_file, "_")[[1]]
-  df <- read.csv(file.path(output_dir, df_file), colClasses=c("NULL","factor","integer","integer","integer"))
-  df$p <- as.factor(params[[2]])
+  df <- read.csv(file.path(output_dir, df_file), colClasses=c("NULL","factor","integer","integer","integer", "numeric"))
+  as <- params[[2]]
+  df$as <- as.factor(as)
   df$ws <- as.factor(params[[3]])
   zs <- params[[4]]
   df$zs <- as.factor(zs)
@@ -47,75 +49,181 @@ for(i in 1:length(df_files)){
   } else {
     df$zs1 <- df$zs2 <- NA
   }
+  if(grepl("pow", as)){
+    asv <- strsplit(as, "-")[[1]]
+    df$as1 <- "pow"
+    df$as2 <- as.factor(asv[2])
+    df$as3 <- as.factor(asv[3])
+  } else if(grepl("rand", as)){
+    df$as1 <- "rand"
+    df$as2 <- as.factor(substr(as, 5, nchar(as)))
+    df$as3 <- NA
+  } else {
+    df$as1 <- "block"
+    df$as2 <- df$as3 <- NA
+  }
+  df$ess <- 0
+  for(method in c("Swap", "Curveball")){
+    # Effective sample size at each step
+    stat <- df[df$method==method,c("stat")]
+    N <- length(stat)
+    ess <- rep(0, N) 
+    for(j in 2:N){ # step j
+      ess[j] <- 1/(1+2*sum(acf(stat[1:j], lag.max=N, plot=F)$acf))
+    }
+    df[df$method==method,c("ess")] <- ess
+  }
   dfs[[i]] <- df
 }
 df <- do.call(rbind, dfs)
+write.csv(df, file=file.path(output_dir, pu("df.csv")))
 
 ### Plots
 
 # weights vs 1's density
 df %>% filter(zs=="none") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~ws, scales="free_y") + 
-  labs(title="Zero Scheme: None")
-ggsave(file.path(output_dir, pu("stat_p_vs_weight-scheme.png")), height=5, width=8)
+  facet_grid(as~ws, scales="free_y") + 
+  labs(title="Diag Statistic, Zero Scheme: None")
+ggsave(file.path(output_dir, pu("diag_as_vs_weight-scheme.png")), height=5, width=8)
+
+df %>% filter(zs=="none") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~ws, scales="free_y") + 
+  labs(title="Heterogeneity Statistic, Zero Scheme: None")
+ggsave(file.path(output_dir, pu("hetero_as_vs_weight-scheme.png")), height=5, width=8)
 
 # Zeros vs 1 density for uniform weights
 df %>% filter(ws=="uniform") %>%
   ggplot(aes(x=iter, y=dissim, color=method)) + geom_line() + 
-  facet_grid(p~zs, scales="free_y") + 
+  facet_grid(as~zs, scales="free_y") + 
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("dissim_p_vs_zero-scheme.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("dissim_as_vs_zero-scheme.png")), height=5, width=8)
 
 # Statistic trajectory for different zeros types
 df %>% filter(zs=="none", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y")
+  facet_grid(as~., scales="free_y") + 
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_none.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_none.png")), height=5, width=8)
 
-df %>% filter(zs=="tri.1", ws=="uniform") %>%
+df %>% filter(zs=="tri.10", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y") + 
+  facet_grid(as~., scales="free_y") + 
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_tri.1.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_tri.10.png")), height=5, width=8)
 
 df %>% filter(zs=="tri.25", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y") +
+  facet_grid(as~., scales="free_y") +
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_tri.25.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_tri.25.png")), height=5, width=8)
 
 df %>% filter(zs=="tri.50", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y") + 
+  facet_grid(as~., scales="free_y") + 
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_tri.50.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_tri.50.png")), height=5, width=8)
 
-df %>% filter(zs=="runif.1", ws=="uniform") %>%
+df %>% filter(zs=="runif.10", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y") + 
+  facet_grid(as~., scales="free_y") + 
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_runif.1.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_runif.1.png")), height=5, width=8)
 
 df %>% filter(zs=="runif.25", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y") +
+  facet_grid(as~., scales="free_y") +
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_runif.25.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_runif.25.png")), height=5, width=8)
 
 df %>% filter(zs=="runif.50", ws=="uniform") %>%
   ggplot(aes(x=iter, y=stat, color=method)) + geom_line() + 
-  facet_grid(p~., scales="free_y") + 
+  facet_grid(as~., scales="free_y") + 
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_by_p_runif.50.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("diag_by_as_runif.50.png")), height=5, width=8)
 
+# heterogeneity trajectory for different zeros types
+df %>% filter(zs=="none", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") + 
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_none.png")), height=5, width=8)
+
+df %>% filter(zs=="tri.10", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") + 
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_tri.10.png")), height=5, width=8)
+
+df %>% filter(zs=="tri.25", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") +
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_tri.25.png")), height=5, width=8)
+
+df %>% filter(zs=="tri.50", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") + 
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_tri.50.png")), height=5, width=8)
+
+df %>% filter(zs=="runif.10", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") + 
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_runif.1.png")), height=5, width=8)
+
+df %>% filter(zs=="runif.25", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") +
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_runif.25.png")), height=5, width=8)
+
+df %>% filter(zs=="runif.50", ws=="uniform") %>%
+  ggplot(aes(x=iter, y=hetero, color=method)) + geom_line() + 
+  facet_grid(as~., scales="free_y") + 
+  labs(title="Weight Scheme: Uniform")
+ggsave(file.path(output_dir, pu("hetero_by_as_runif.50.png")), height=5, width=8)
 
 # asdf
 
 df %>% filter(ws=="uniform") %>%
   ggplot(aes(x=iter, y=dissim, color=zs2, linetype=method)) + geom_line() + 
-  facet_grid(p~zs1, scales="free_y")
+  facet_grid(as~zs1, scales="free_y")
   labs(title="Weight Scheme: Uniform")
-ggsave(file.path(output_dir, pu("stat_p_vs_zero-scheme.png")), height=5, width=8)
+ggsave(file.path(output_dir, pu("stat_as_vs_zero-scheme.png")), height=5, width=8)
 
+
+    
+# my failed attempt at computing ESS more efficiently
+if(F){
+  x12cs <- list()
+  x1x2cs <- list()
+  for(l in 2:(N-1)){
+    x12cs[[l]] <- cumsum(stat[1:(N-l)]*stat[(1+l):N])
+    x1x2cs[[l]] <- cumsum(stat[1:(N-l)]+stat[(1+l):N])
+  }
+  xbar <- 0
+  for(j in 9990:1000){
+    print(j)
+    xbar <- (xbar*(j-1)+stat[j])/j
+    SS <- numeric(N-2)
+    for(l in 2:(N-1)){
+      SS[l] <- x12cs[[l]][j] + x1x2cs[[l]][j]*xbar + xbar^2
+    }
+    acvf <- SS/j
+    acf <- if(acvf[1]!=0){acvf/acvf[1]}else{1}
+    ess[j] <- 1/(1+2*sum(acf))
+  }
+  # my other failed attempt:  
+  for(j in 2:(N-j)){ # step j
+      xbar <- (xbar*(j-1)+df$stat[j])/j
+      acvfs <- numeric(j-1)
+      for(l in 1:(j-1)){ # lag l
+        acvf <- 1/j*sum((df$stat[1:(j-l)]-xbar)*(df$stat[l:j]-xbar))
+      }
+      acf <- if(acvf[1]!=0){acvf/acvf[1]}else{1}
+      df$stat_ess[j] <- ess <- 1/(1+2*sum(acf))
+    }
+}
