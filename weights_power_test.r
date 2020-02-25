8964e0645e9195739e9cc91b9f4cfc69a6159700
Alex Fout <fout.alex@gmail.com>
2020-02-24 20:44:48 -0700

    migrated functions to functions.r and added saveRDS commands for A and W

--- weights_power_test.r
+++ weights_power_test.r
@@ -16,31 +16,10 @@ library(reshape2)
 library(ggplot2)
 
 
-heat <- function(X){
-  df <- melt(X)
-  ggplot(df, aes(x=Var1, y=Var2)) + 
-    geom_tile(aes(fill=value)) + scale_fill_gradient2() 
-}
 output_dir <- file.path("output", "weights_power")
 dir.create(output_dir, showWarnings=F)
-
 pu <- function(...){
   paste("wp", ..., sep="_")
-}
-
-diag_stat <- function(X){
-  ones <- which(X==1, arr.ind=T)
-  return(sum(abs(ones[,1] - ones[,2])))
-}
-hetero_stat <- function(Xs){
-  # heterogeneity of the matrix
-  # This is a cumulative statistic, so it needs full state history to compute
-  p <- mean(Xs[,,1])  # density of the matrix (unchanged)
-  N <- dim(Xs)[3]
-  nm <- prod(dim(Xs)[1:2])
-  Xsc <- aperm(apply(Xs, 1:2, cumsum), c(2,3,1))  # cumsum each element
-  thetas <- sapply(1:N, function(i){sum((Xsc[,,i]/i - p)^2)/nm})
-  return(thetas)
 }
 
 # sampling parameters
@@ -63,16 +42,13 @@ zeros <- matrix(0, n/2, m/2)
 ones <- matrix(1, n/2, m/2)
 A <- rbind(cbind(zeros, ones), cbind(ones, zeros)); heat(A)
 ggsave(pu("A.png"), path=output_dir, height=4, width=5)
+saveRDS(A, file.path(output_dir, pu("A.rds")))
+
 W <- outer(1:n, 1:m, FUN=function(x, y){n+m-abs(x-y)})/(n+m); heat(W)
 ggsave(pu("W.png"), path=output_dir, height=4, width=5)
-#W1 <- outer(1:n, 1:m, FUN=function(x, y){n+m-abs(x-y)})/(n+m); heat(W1)
-#ggsave(pu("W1.png"), path=output_dir, height=4, width=5)
-#W2 <- outer(1:n, 1:m, FUN=function(x, y){abs(x-y)})/(n+m)+0.5; heat(W2)
-#ggsave(pu("W2.png"), path=output_dir, height=4, width=5)
-
+saveRDS(W, file.path(output_dir, pu("W.rds")))
 
 # sample:
-
 sim <- function(p){
   cat(paste("p =", p, "\t"))
   A2 <- A
@@ -109,4 +85,3 @@ ggplot(df, aes(x=stat, y=..count.., fill=p)) +
   geom_histogram(alpha=0.4, color="black", bins=100, position="identity") +
   labs(title=bquote('Sampling under weights'~W^p), x="Statistic", y="Frequency")
 ggsave(pu("sample.png"), path=output_dir, width=12, height=5)
-
